cfe788dbfd2f1b140856b3ba8c4473a57b4935fd
Kelson <kelsonbatista@gmail.com>
2024-07-06 14:25:24 -0300

    Introduction

---
+++ 00 - introduction.py
@@ -0,0 +1,18 @@
+# Install 'python3'
+# Ao digitar python3 e enter, ira entrar no terminal, ja podendo executar o codigo
+# ex. 4*5 > enter > 20
+# a = 'teste'  - variavel
+# ao dar enter em 'a' aparece o valor da variavel
+# type(a) - vai mostrar que é uma variavel do tipo string
+# help() vai entrar no terminal de ajuda e para sair clicar em 'q' (quit)
+# Ctrl + D sai do terminal principal
+
+# Instalar as extensoes do vscode - Pyhton (ao instalar a principal, ja é instalado tb a pylens)
+# Instalar outros utilitarios também
+# pip = package installer for python
+# Para instalar o pip = curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py; python3 get-pip.py
+# ou python -m pip install --upgrade pip 
+# ou apt-get install python3-pip
+
+# pip install black  - formata o arquivo automaticamente e deixa mais legivel
+# pip install flake8 - acusa problemas no codigo
