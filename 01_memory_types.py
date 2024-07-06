40534b3c7098323181c97366921fb9839fb1c269
Kelson <kelsonbatista@gmail.com>
2024-07-06 14:54:16 -0300

    Memory and types

---
+++ 01_memory_types.py
@@ -0,0 +1,12 @@
+# Python é uma linguagem de programação dinamica e interpretada
+# É executada diretamente linha por linha por um interpretador (e não compilada)
+# O interpretador é um console ou terminal, no caso é o 'python3'. No caso do Linux é nativo.
+# Atraves do comando, percebmos que ele utiliza a memoria para armazenar , ler e posteriormente processar os dados
+
+# Comando type para saber o tipo do objeto
+print(type(35))
+print(type(35 + 5))
+print(type('#'))
+print('#' + 'bigdata')
+print('#' + 35)
+
